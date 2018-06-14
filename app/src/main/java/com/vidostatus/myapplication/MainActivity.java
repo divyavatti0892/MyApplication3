c86408b6a24d06eaabccb567d9a7a749b9b9f239
divya <divyavatti0892@gmail.com>
2018-06-14 14:35:18 +0530

    Commit New

--- app/src/main/java/com/vidostatus/myapplication/MainActivity.java
+++ app/src/main/java/com/vidostatus/myapplication/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Test
+        // Sjjjdfdnmdsnds;
+        // Testing GIt
     }
 }
---
+++ app/src/main/java/com/vidostatus/myapplication/N.java
@@ -0,0 +1,8 @@
+package com.vidostatus.myapplication;
+
+/**
+ * Created by dines on 14-06-2018.
+ */
+
+public class N {
+}
